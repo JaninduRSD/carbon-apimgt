85698f3bdfba1d29abccb17c1597446884771687
JaninduRSD <janidu.22@cse.mrt.ac.lk>
2026-03-09 23:42:48 +0530

    Implement APIDefinitionProcessor for handling OpenAPI and AsyncAPI specifications

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/APIDefinitionProcessorFactory.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/APIDefinitionProcessorFactory.java
@@ -41,7 +41,7 @@ public class APIDefinitionProcessorFactory {
      * @return The corresponding definition processor implementation.
      */
     public static APIDefinitionProcessor getDefinitionProcessor(API api) {
-        if (api != null && api.isAsync()) {
+        if (api != null && (api.isAsync() || "WEBHOOK".equalsIgnoreCase(api.getType()))) {
             return ASYNC_PROCESSOR;
         }
         return OAS_PROCESSOR;
@@ -57,7 +57,8 @@ public class APIDefinitionProcessorFactory {
         if ("WS".equalsIgnoreCase(apiType) ||
             "WEBSUB".equalsIgnoreCase(apiType) ||
             "SSE".equalsIgnoreCase(apiType) ||
-            "ASYNC".equalsIgnoreCase(apiType)) {
+            "ASYNC".equalsIgnoreCase(apiType) ||
+            "WEBHOOK".equalsIgnoreCase(apiType)) {
             return ASYNC_PROCESSOR;
         }
         return OAS_PROCESSOR;
